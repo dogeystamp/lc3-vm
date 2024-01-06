0597b1d4f818e7f80e9f48380a6e9b4b5ab9a991
dogeystamp <dogeystamp@disroot.org>
2024-01-06 14:05:41 -0500

    command line argument for program

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@
 
 mod vm;
 use crate::vm::VM;
+use std::env;
 
 fn main() {
-    let mut vm = VM::new();
+    let args : Vec<_> = env::args().collect();
 
-    vm.read_program(&"programs/hello-world.obj".to_string());
+    let mut vm = VM::new();
+    vm.read_program(args.get(1).expect("No program file given"));
     vm.execute();
 }
